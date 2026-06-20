e5d3cad1b5009aa909ce5a8929964f5ca3113af4
code_overflowed <neerajsingla380@gmail.com>
2026-06-20 14:22:08 +0530

    Template inheritance added

---
+++ Flask/03_template_inheritance.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+
+web = Flask(__name__)
+
+@web.route("/")
+def hello():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    web.run(debug=True)
