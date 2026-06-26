3567621518bf193880aa545393933574194b416e
code_overflowed <neerajsingla380@gmail.com>
2026-06-26 18:49:30 +0530

    Getting the data

--- Web-Scraping/web.py
+++ Web-Scraping/web.py
@@ -1,6 +1,9 @@
-import urllib.request ,urllib.parse,urllib.error
+import requests
 
-url = urllib.request.urlopen('http://google.com')
-
-for line in url:
-    print(line.decode().strip())
+url ='http://127.0.0.1:8000/'
+user ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36'}
+# print(dir(requests)) 
+response=requests.get(url=url,headers=user)
+print(response)
+print(response.headers)
+print(response.request.headers) 
