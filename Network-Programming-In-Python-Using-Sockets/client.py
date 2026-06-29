329e9a0c3ba5cc2ac79f112a54a9636dd2628194
code_overflowed <neerajsingla380@gmail.com>
2026-06-29 23:33:45 +0530

    Completing Our Chat App

--- Network-Programming-In-Python-Using-Sockets/client.py
+++ Network-Programming-In-Python-Using-Sockets/client.py
@@ -1,4 +1,33 @@
 import socket
+from tkinter import *
+
+def send(list_box,entry):
+    message=entry.get()
+    list_box.insert(END,'Client: '+message)
+    entry.delete(0,END)
+    s.send(bytes(message,'utf-8'))
+
+def receive(list_box):
+    message = s.recv(50)
+    list_box.insert(END,'Server: '+message.decode('utf-8'))
+
+root =Tk()
+entry =Entry(root)
+entry.pack(side=BOTTOM)
+
+list_box=Listbox(root)
+list_box.pack()
+
+send_btn=Button(root,text='Send',command=lambda:send(list_box,entry))
+send_btn.pack()
+
+receive_btn=Button(root,text='Receive',command=lambda:receive(list_box))
+receive_btn.pack()
+
+root.minsize(400, 300)
+root.maxsize(800, 600)
+
+root.title('Client')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = socket.gethostname()
@@ -6,8 +35,4 @@ port = 12345
 s.connect((IP, port))
 print("Connected to server!")
 
-while True:
-    message_from_server = s.recv(50)
-    print("Server: ",message_from_server.decode('utf-8'))
-    message_to_send=input("Client: ")
-    s.send(bytes(message_to_send,'utf-8'))
+root.mainloop()
--- Network-Programming-In-Python-Using-Sockets/server.py
+++ Network-Programming-In-Python-Using-Sockets/server.py
@@ -1,16 +1,45 @@
 import socket
 
+from tkinter import *
+
+def send(list_box,entry):
+    message=entry.get()
+    list_box.insert(END,'Server: '+message)
+    entry.delete(0,END)
+    client.send(bytes(message,'utf-8'))
+
+def receive(list_box):
+    message_from_client = client.recv(50)
+    list_box.insert(END,'Client: '+message_from_client.decode('utf-8'))
+
+root =Tk()
+entry =Entry(root)
+entry.pack(side=BOTTOM)
+
+list_box=Listbox(root)
+list_box.pack()
+
+send_btn=Button(root,text='Send',command=lambda:send(list_box,entry))
+send_btn.pack()
+
+receive_btn=Button(root,text='Receive',command=lambda:receive(list_box))
+receive_btn.pack()
+
+root.minsize(400, 300)
+root.maxsize(800, 600)
+
+root.title('Server')
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = socket.gethostname()
 port = 12345
+
 s.bind((IP, port))
 s.listen(4)
-print("Server is waiting for connections...")
-client,address = s.accept()
 
-while True:
-    message = input("Server: ")
-    client.send(bytes(message,'utf-8'))
-    message_from_client=client.recv(50)
-    print("Client: ",message_from_client.decode('utf-8'))
- 
+print("Server is waiting for connections...")
+
+client, address = s.accept()
+
+  
+root.mainloop()
